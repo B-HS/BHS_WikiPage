1e1f68acc6cd8e145165370a93e288e6abb5e183
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-15 02:09:34 +0900

    Add searchparam for back to list button

--- components/header/navagation.tsx
+++ components/header/navagation.tsx
@@ -1,11 +1,17 @@
+'use client'
+import Link from 'next/link'
 import Button from '../button'
+import { useSearchParams } from 'next/navigation'
 
 const Navigation = () => {
+    const searchParams = useSearchParams()
     return (
         <section className='flex flex-wrap space-x-2'>
-            <Button size='md' variant='ghost'>
-                목록
-            </Button>
+            <Link href={`/?page=${searchParams.get('page') || '1'}`}>
+                <Button size='md' variant='ghost'>
+                    목록
+                </Button>
+            </Link>
             <Button size='md' variant='ghost'>
                 추가
             </Button>
