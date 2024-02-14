ac5b5275b07091bb981091065020ca1cda9678d6
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-14 21:50:05 +0900

    Add dummy list for documents list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import List from '@/components/list/list'
+import { DocProps } from './doc/[docid]/page'
+
 const Home = () => {
-    return <span>HELLO</span>
+    const dummy = Array.from({ length: 30 }).map((_, idx) => ({ docid: idx, title: `title${idx}`, description: `description${idx}` })) as DocProps[]
+    return (
+        <section className='flex flex-col flex-1'>
+            <List docs={dummy} />
+        </section>
+    )
 }
 
 export default Home
