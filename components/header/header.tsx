4f275c9c230fcd3fc181fddce1ff47617feb2a8c
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-15 02:09:54 +0900

    Fix logo button to raw anchor tag to refresh the page

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link'
 import Navigation from './navagation'
 
 const Header = () => {
     return (
         <header className='sticky top-0 z-50 w-full border-b backdrop-blur'>
             <section className='flex h-14 justify-between items-center px-3'>
-                <Link href={'/'} className='flex gap-2 items-center text-xl font-bold'>
+                <a href={'/'} className='flex gap-2 items-center text-xl font-bold'>
                     위키
-                </Link>
+                </a>
                 <Navigation />
             </section>
         </header>
