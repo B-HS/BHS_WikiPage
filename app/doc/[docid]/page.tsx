ab9b09a11af33515e69a4f228297f7b086d24b12
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-15 02:10:23 +0900

    Working on Document page

--- app/doc/[docid]/page.tsx
+++ app/doc/[docid]/page.tsx
@@ -1,11 +1,29 @@
+import { findDocByDocid } from '@/dummydocs/service'
+import { headers } from 'next/headers'
+
 export interface DocProps {
     docid: number | string
     title: string
     description: string
 }
 
-const Document = ({ params }: { params: { docid: string } }) => {
-    return <section> Document {params.docid} </section>
+const Document = async ({ params, searchParams }: { params: { docid: string }; searchParams: { [key: string]: string } }) => {
+    const headersList = headers()
+    const domain = headersList.get('x-forwarded-host')
+    const origin = headersList.get('x-forwarded-proto')
+    const currentURL = `${origin}://${domain}`
+    const fetchData = await fetch(`${currentURL}/api/doc/${params.docid}?page=${searchParams['page']}`)
+    const { title, docid, description } = await fetchData.json()
+
+    return (
+        <section>
+            <span>- {docid}</span>
+            <hr />
+            <p>{title}</p>
+            <hr />
+            <div dangerouslySetInnerHTML={{ __html: description }}></div>
+        </section>
+    )
 }
 
 export default Document
