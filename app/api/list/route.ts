c80880f59bc001b3afd34c9380bd76ba0a700113
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-16 01:58:59 +0900

    Remove unused parameter

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,11 +1,10 @@
 import { findDocs } from '@/dummydocs/controller'
 import { NextRequest, NextResponse } from 'next/server'
 
-export const GET = async (request: NextRequest, { params }: { params: { docid: string } }) => {
+export const GET = async (request: NextRequest) => {
     const COUNT = 5
     const url = new URL(request.url)
     const page = parseInt(url.searchParams.get('page') || '1', 10)
-
     const docs = await findDocs(page, COUNT)
     return NextResponse.json(docs)
 }
