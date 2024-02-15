3bd18a566edbfb0addccd05b1e08daaa03b18f8c
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-16 02:01:01 +0900

    Add First/Last button for pagination

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react'
 import Button from './button'
 
 const Pagination = ({ currentPage, totalPages, onPageChange }: { currentPage: number; totalPages: number; onPageChange: (pageNumber: number) => void }) => {
@@ -21,13 +21,22 @@ const Pagination = ({ currentPage, totalPages, onPageChange }: { currentPage: nu
         <section className='mx-auto p-3'>
             <section className='flex-1 flex justify-center'>
                 {currentPage !== 1 && (
-                    <Button
-                        variant='outline'
-                        onClick={() => onPageChange(currentPage - 5 < 1 ? 1 : currentPage - 5)}
-                        className={`${currentPage === 1 ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'} rounded-none `}
-                    >
-                        <ChevronLeft className='w-3.5 h-3.5' />
-                    </Button>
+                    <>
+                        <Button
+                            variant='outline'
+                            onClick={() => onPageChange(1)}
+                            className={`${currentPage === 1 ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'} rounded-none `}
+                        >
+                            <ChevronsLeft className='w-3.5 h-3.5' />
+                        </Button>
+                        <Button
+                            variant='outline'
+                            onClick={() => onPageChange(currentPage - 5 < 1 ? 1 : currentPage - 5)}
+                            className={`${currentPage === 1 ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'} rounded-none `}
+                        >
+                            <ChevronLeft className='w-3.5 h-3.5' />
+                        </Button>
+                    </>
                 )}
                 <section>
                     {pageNumbers.map((number) => (
@@ -42,13 +51,22 @@ const Pagination = ({ currentPage, totalPages, onPageChange }: { currentPage: nu
                     ))}
                 </section>
                 {currentPage !== totalPages && (
-                    <Button
-                        variant='outline'
-                        onClick={() => onPageChange(currentPage + 5 > totalPages ? totalPages : currentPage + 5)}
-                        className={`${currentPage === totalPages ? 'cursor-not-allowed' : 'cursor-pointer'} rounded-none `}
-                    >
-                        <ChevronRight className='w-3.5 h-3.5' />
-                    </Button>
+                    <>
+                        <Button
+                            variant='outline'
+                            onClick={() => onPageChange(currentPage + 5 > totalPages ? totalPages : currentPage + 5)}
+                            className={`${currentPage === totalPages ? 'cursor-not-allowed' : 'cursor-pointer'} rounded-none `}
+                        >
+                            <ChevronRight className='w-3.5 h-3.5' />
+                        </Button>
+                        <Button
+                            variant='outline'
+                            onClick={() => onPageChange(totalPages)}
+                            className={`${currentPage === totalPages ? 'cursor-not-allowed' : 'cursor-pointer'} rounded-none `}
+                        >
+                            <ChevronsRight className='w-3.5 h-3.5' />
+                        </Button>
+                    </>
                 )}
             </section>
         </section>
