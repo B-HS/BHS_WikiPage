46befeb16898e853a4d1ced5f0aec43e28e45538
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-17 03:35:42 +0900

    Simplifying compare logic for pagination component

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -23,14 +23,14 @@ const Pagination = ({ currentPage, totalPages, onPageChange }: { currentPage: nu
                         <Button
                             variant='outline'
                             onClick={() => onPageChange(1)}
-                            className={`${currentPage === 1 ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'} rounded-none `}
+                            className={`${currentPage === 1 ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'} rounded-none`}
                         >
                             <ChevronsLeft className='w-3.5 h-3.5' />
                         </Button>
                         <Button
                             variant='outline'
-                            onClick={() => onPageChange(currentPage - 5 < 1 ? 1 : currentPage - 5)}
-                            className={`${currentPage === 1 ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'} rounded-none `}
+                            onClick={() => onPageChange(Math.max(1, currentPage - 5))}
+                            className={`${currentPage === 1 ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'} rounded-none`}
                         >
                             <ChevronLeft className='w-3.5 h-3.5' />
                         </Button>
@@ -42,7 +42,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }: { currentPage: nu
                             variant={currentPage === number ? 'outlineInverted' : 'outline'}
                             key={number}
                             onClick={() => onPageChange(number)}
-                            className={`${currentPage === number ? 'hover:bg-foreground hover:border-black hover:text-white' : ''} rounded-none `}
+                            className={`${currentPage === number ? 'hover:bg-foreground hover:border-black hover:text-white' : ''} rounded-none`}
                         >
                             {number}
                         </Button>
@@ -52,15 +52,15 @@ const Pagination = ({ currentPage, totalPages, onPageChange }: { currentPage: nu
                     <>
                         <Button
                             variant='outline'
-                            onClick={() => onPageChange(currentPage + 5 > totalPages ? totalPages : currentPage + 5)}
-                            className={`${currentPage === totalPages ? 'cursor-not-allowed' : 'cursor-pointer'} rounded-none `}
+                            onClick={() => onPageChange(Math.min(totalPages, currentPage + 5))}
+                            className={`${currentPage === totalPages ? 'cursor-not-allowed' : 'cursor-pointer'} rounded-none`}
                         >
                             <ChevronRight className='w-3.5 h-3.5' />
                         </Button>
                         <Button
                             variant='outline'
                             onClick={() => onPageChange(totalPages)}
-                            className={`${currentPage === totalPages ? 'cursor-not-allowed' : 'cursor-pointer'} rounded-none `}
+                            className={`${currentPage === totalPages ? 'cursor-not-allowed' : 'cursor-pointer'} rounded-none`}
                         >
                             <ChevronsRight className='w-3.5 h-3.5' />
                         </Button>
