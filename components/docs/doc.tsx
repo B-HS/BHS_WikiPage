c860ed9dac3673e7e33fa9df482fda5224cab417
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-17 03:25:38 +0900

    Fix styling for doc detail page

--- components/docs/doc.tsx
+++ components/docs/doc.tsx
@@ -6,12 +6,12 @@ const Doc = async ({ doc }: { doc: DocProps }) => {
     const { title, description } = doc
 
     return (
-        <section className='grid grid-cols-1 divide-y my-7'>
+        <section className='grid grid-cols-1 divide-y'>
             <section className='flex items-center gap-2 py-7'>
                 <GoBackButton />
                 <span className='text-3xl font-extrabold'>{title}</span>
             </section>
-            <div className='py-7' dangerouslySetInnerHTML={{ __html: description }} />
+            <div className='py-7 break-words' dangerouslySetInnerHTML={{ __html: description }} />
             <DocumentButtons info={doc} />
         </section>
     )
