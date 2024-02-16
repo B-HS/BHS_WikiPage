56271e732948fa21f26214529304b720601e746c
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-17 03:26:46 +0900

    Add CUD controller logic

--- dummydocs/controller.ts
+++ dummydocs/controller.ts
@@ -1,4 +1,5 @@
-import { findDocByDocid, findDocsWithPageable } from './service'
+import { DocProps } from '@/app/doc/[docid]/page'
+import { deleteDoc, findDocByDocid, findDocsWithPageable, insertDoc, updateDoc } from './service'
 
 export const findDocs = (page: number, cnt: number) => {
     return findDocsWithPageable(page, cnt)
@@ -6,4 +7,16 @@ export const findDocs = (page: number, cnt: number) => {
 
 export const findDocById = (docid: string, page: number | string) => {
     return findDocByDocid(docid, page)
+}
+
+export const writeDoc = (doc: DocProps) => {
+    return insertDoc(doc)
+}
+
+export const modifyDoc = (doc: DocProps) => {
+    return updateDoc(doc)
+}
+
+export const removeDoc = (docid: string) => {
+    return deleteDoc(docid)
 }
