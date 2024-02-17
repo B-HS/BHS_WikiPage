49057410c4fb789013b5241fe1a19af8ddbaa05a
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-18 02:26:05 +0900

    Fix searchparam to using URLSearchParams fn

--- app/doc/[docid]/page.tsx
+++ app/doc/[docid]/page.tsx
@@ -1,6 +1,7 @@
 import { PageSetter } from '@/context/contexts/page-context'
 import { headers } from 'next/headers'
 import Doc from '../../../components/docs/doc'
+import { URLSearchParams } from 'url'
 
 export interface DocProps {
     docid: number | string
@@ -8,12 +9,13 @@ export interface DocProps {
     description: string
 }
 
-const Document = async ({ params, searchParams }: { params: { docid: string }; searchParams: { page: string } }) => {
+const Document = async ({ params, searchParams }: { params: { docid: string }; searchParams: { page: string; keyword?: string } }) => {
     const headersList = headers()
     const domain = headersList.get('x-forwarded-host')
     const origin = headersList.get('x-forwarded-proto')
     const currentURL = `${origin}://${domain}`
-    const fetchData = await fetch(`${currentURL}/api/doc/${params.docid}?page=${searchParams.page}`)
+    const searchPraramString = new URLSearchParams(searchParams).toString()
+    const fetchData = await fetch(`${currentURL}/api/doc/${params.docid}?${searchPraramString}`)
     const { title, docid, description } = await fetchData.json()
     return (
         <>
