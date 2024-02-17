75f2027122acfadeecffa1ad06417e4dd3251cd1
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-18 02:25:23 +0900

    Fix read wiki api for able to get keyword

--- app/api/doc/[docid]/route.ts
+++ app/api/doc/[docid]/route.ts
@@ -2,7 +2,10 @@ import { findDocById } from '@/dummydocs/controller'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest, { params }: { params: { docid: string } }) => {
-    const page = request.url.split('?')[1]?.split('=')[1] || 1
-    const docs = await findDocById(params.docid, page)
+    const searchParamObj = new URLSearchParams(request.url)
+    const page = Number(searchParamObj.get('page')) || 1
+    const keyword = searchParamObj.get('keyword') || undefined
+
+    const docs = await findDocById(params.docid, { page, keyword })
     return NextResponse.json(docs)
 }
