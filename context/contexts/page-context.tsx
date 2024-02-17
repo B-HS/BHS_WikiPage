10369936b5c755c9376b989b16ad25e0926ca02a
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-18 02:28:27 +0900

    Add variables to page context

--- context/contexts/page-context.tsx
+++ context/contexts/page-context.tsx
@@ -4,25 +4,33 @@ import { Dispatch, SetStateAction, createContext, useContext, useEffect, useStat
 
 interface PageContextType {
     page: number
+    totalPages: number
+    keyword: string
     setPage: Dispatch<SetStateAction<number>>
+    setTotalPages: Dispatch<SetStateAction<number>>
+    setKeyword: Dispatch<SetStateAction<string>>
 }
 
-export const PageContext = createContext<PageContextType>({ page: 1, setPage: () => {} })
+export const PageContext = createContext<PageContextType>({ page: 1, totalPages: 0, keyword: '', setPage: () => {}, setTotalPages: () => {}, setKeyword: () => {} })
 
 export const PageSetter = () => {
     const searchParams = useSearchParams()
-    const { setPage } = useContext(PageContext)
+    const { setPage, setKeyword } = useContext(PageContext)
     useEffect(() => {
         const page = searchParams.get('page')
+        const keyword = searchParams.get('keyword')
         page && setPage(Number(page) || 1)
-    }, [searchParams, setPage])
+        keyword && setKeyword(keyword)
+    }, [searchParams, setPage, setKeyword])
     return null
 }
 
 const PageContextProvider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     const [page, setPage] = useState<number>(1)
+    const [totalPages, setTotalPages] = useState<number>(0)
+    const [keyword, setKeyword] = useState<string>('')
 
-    return <PageContext.Provider value={{ page, setPage }}>{children}</PageContext.Provider>
+    return <PageContext.Provider value={{ page, totalPages, keyword, setPage, setTotalPages, setKeyword }}>{children}</PageContext.Provider>
 }
 
 export default PageContextProvider
