3742c570dda90b05590bd7aa9a91fa5f45dec542
Hyunseok Byun <gumyoincirno@gmail.com>
2024-02-18 02:25:41 +0900

    Fix list wiki api for able to get keyword

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,10 +1,12 @@
 import { findDocs } from '@/dummydocs/controller'
+import { DEFAULT_PAGING_CNT } from '@/util/constant'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest) => {
-    const COUNT = 5
+    const COUNT = DEFAULT_PAGING_CNT
     const url = new URL(request.url)
     const page = parseInt(url.searchParams.get('page') || '1', 10)
-    const docs = await findDocs(page, COUNT)
+    const keyword = url.searchParams.get('keyword') || undefined
+    const docs = await findDocs({ keyword, page }, COUNT)
     return NextResponse.json(docs)
 }
